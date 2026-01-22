2b07aefeea15013652d76ae20a0c27c453a98b8c
charan das <nanitalari21@gmail.com>
2026-01-22 14:51:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:'/Personal-Portfolio/',
-  build: {
-    outDir: 'docs',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  },
+  base:'/Personal-Portfolio',
 }
-)
+);
