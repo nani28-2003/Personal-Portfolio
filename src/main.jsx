6bbbf9a01ae0e16c91c0c4e70786ebbbf3455fc1
charan das <nanitalari21@gmail.com>
2026-01-22 19:58:30 +0530

    modified-portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import React from 'react'
 import ReactDom from 'react-dom/client'
 const { createRoot } = ReactDom;
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter >
+    <HashRouter >
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </StrictMode>,
 )
