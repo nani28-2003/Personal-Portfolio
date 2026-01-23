5a6bb713900bc4cda8d381ab5e0cc561bbf730a8
charan das <nanitalari21@gmail.com>
2026-01-24 01:10:47 +0530

    strict-commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ import "./index.css";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <HashRouter >
     <App />
   </HashRouter>
-  </StrictMode>,
+  
 )
